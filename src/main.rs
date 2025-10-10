f220f8f1d002e480f4d3f780e2989d4e5b54507c
haikalnuril <haikal.nuril23@gmail.com>
2025-10-11 06:08:28 +0700

    Add test functions for variable handling and conversions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello_test(){
+    println!("Hello, test!");
+}
+
+#[test]
+fn test_variable(){
+    let name = "Haikal Nuril";
+    println!("Hello, {}", name);
+}
+
+#[test]
+fn test_mutable_variable(){
+    let mut name = "Haikal Nuril";
+    println!("Hello, {}", name);
+    name = "Uriel";
+    println!("Hello, {}", name);
+}
+
+#[test]
+fn shadowing_variable(){
+    let name = "Haikal Nuril";
+    println!("Hello, {}", name);
+    let name = 10;
+    println!("Hello, {}", name);
+}
+
+#[test]
+fn explicit(){
+    let age: i32 = 20;
+    println!("Age: {}", age);
+}
+
+#[test]
+fn number_conversion(){
+    let age: i32 = 20;
+    println!("Age: {}", age);
+    let age2: i64 = age as i64;
+    println!("Age: {}", age2);
+    let age3: i128 = age2 as i128;
+    println!("Age: {}", age3);
+
+    //integer overflow
+    let d: i64 = 1000000000;
+    let e: i8 = d as i8;
+    println!("e: {}", e); //e: 0 (because of overflow)
+}
