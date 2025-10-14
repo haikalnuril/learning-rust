cc98909865e48fbc8c0d52cd31202e27dc237fae
haikalnuril <haikal.nuril23@gmail.com>
2025-10-14 16:19:18 +0700

    Add tests for arrays, constants, variable scope, stack and heap, strings, and ownership rules

--- src/main.rs
+++ src/main.rs
@@ -101,3 +101,110 @@ fn test_unit(){
     let test: () = ();
     println!("Test: {:?}", test);
 }
+
+#[test]
+fn array(){
+    let array: [i32; 3] = [1, 2, 3];
+    println!("Array: {:?}", array);
+    println!("Array[0]: {}", array[0]);
+    println!("Array length: {}", array.len());
+
+    let mut array: [i32; 3] = [1, 2, 3];
+    array[0] = 10;
+    println!("Array: {:?}", array);
+}
+
+const MAX: i32 = 100;
+
+#[test]
+fn constant(){
+    const MIN: i32 = 0;
+    println!("MAX: {}", MAX);
+    println!("MIN: {}", MIN);
+}
+
+#[test]
+fn variable_scope(){
+    let name = "Haikal Nuril";
+    println!("Hello, {}", name);
+    {
+        let name = "Uriel";
+        println!("Hello, {}", name);
+        let age = 20;
+    }
+    println!("Hello, {}", name);
+    // println!("Age: {}", age); //error: age is not found
+}
+
+#[test]
+fn stack_and_heap(){
+    function_a();
+    function_b();
+}
+
+fn function_a(){
+    let a = 10;
+    let b = String::from("haikal");
+    println!("a: {}, b: {}", a, b);
+}
+
+fn function_b(){
+    let a = 10;
+    let b = String::from("ril");
+    println!("a: {}, b: {}", a, b);
+}
+
+#[test]
+fn string(){
+    let name: &str = " Haikal Nuril  "; //stack
+    let trim: &str = name.trim();
+
+    println!("Name: {}", name);
+    println!("Trim: {}", trim);
+
+    let mut username = "Eko";
+    username = "Budi";
+    println!("Username: {}", username);
+}
+
+#[test]
+fn string_type(){
+    let mut name: String = String::from("Haikal Nuril"); //heap
+    name.push_str(" Abiyit");
+    println!("Name: {}", name);
+
+    let budi = name.replace("Nuril", "Budi");
+    println!("Budi: {}", budi);
+}
+
+#[test]
+fn ownership_rules() {
+    let a = 10;
+
+    {
+        let b = 20;
+
+        println!("b: {}", b);
+    }
+    // println!("a: {}", b); cant access b here 
+    println!("a: {}", a);
+}
+
+#[test]
+fn ownership_movement() {
+    let name = String::from("Haikal Nuril"); //why cant move? cause its store in heap. so its not implement copy trait data
+
+    // ownership moved to name2
+    let name2 = name;
+    // println!("name: {}", name); cant access name here
+    println!("name2: {}", name2);
+}
+
+#[test]
+fn clone() {
+    let name1 = String::from("Haikal Nuril");
+    let name2 = name1.clone(); // deep copy, this will make big performance issue if the data is too big cause if the first data 100 mb it will copy all the data to the new variable
+
+    println!("name1: {}", name1);
+    println!("name2: {}", name2);
+}
